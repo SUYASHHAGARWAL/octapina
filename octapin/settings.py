43e92b25947051f3fc4ef326b52641cfebe75c78
Suyashh Agarwal <110186843+SUYASHHAGARWAL@users.noreply.github.com>
2023-08-09 22:59:59 +0530

    Update settings.py

--- octapin/settings.py
+++ octapin/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=^w5(!aa3o82y&617bbrs)!(%+d73wspk@s(ip_&ta*2#ooe7^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.236.184.75']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
