a2550d9ee9101925d7f0348d8a466d1bcfad0429
suyashhagarwal <suyash1606.agarwal@gmail.com>
2023-08-12 17:50:14 +0530

    drgrtfgh

--- octapin/settings.py
+++ octapin/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'http://localhost:8000',
+    'https://127.0.0.1:8000',
+    'https://localhost:8000',
     'https://89.117.188.103',
     'https://13.236.184.75',
 ]
